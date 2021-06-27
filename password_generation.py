c91446dfb344db809944696b0cee82e617881dbe
Samuel Freitas <swguitarra@gmail.com>
2021-06-27 15:28:54 -0300

    Segundo commit

--- password_generation.py
+++ password_generation.py
@@ -2,7 +2,7 @@ import random
 lower = "abcdefghijklmnopqrstuvxz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVXZ"
 num = "0123456789"
-sybol = "@#$%¨&*"
+sybol = "!@#$%&*"
 all = lower + upper + num + sybol
 # no length selecionar quantos caracteres vão ter o password
 length = 10
